5f2a5d3bedfc0a224adce28c6dba6fe4a7bb95ad
PeterM1 <MarchToOctober@gmail.com>
2018-04-18 15:42:47 -0400

    edit GameStateManager and EscapeRouteMain

--- core/src/com/templecis/escaperoute/EscapeRouteMain.java
+++ core/src/com/templecis/escaperoute/EscapeRouteMain.java
@@ -14,22 +14,26 @@ import com.templecis.escaperoute.screens.DirectedGame;
 import com.templecis.escaperoute.screens.MenuScreen;
 import com.templecis.escaperoute.screens.transitions.ScreenTransition;
 import com.templecis.escaperoute.screens.transitions.ScreenTransitionSlice;
+import com.templecis.escaperoute.States.*;
 
 public class EscapeRouteMain extends ApplicationAdapter {
 	public static final int WIDTH = 400;
 	public static final int HEIGHT = 800;
 
 	public static final String title = "Escape Route";
-
-	SpriteBatch batch;
+    private GameStateManager gsm;
+	private SpriteBatch batch;
 	Texture img;
 
 	@Override
 	public void create() {
 
 		batch = new SpriteBatch();
+		gsm = new GameStateManager();
 		img = new Texture("badlogic.jpg");
-		// Set Libgdx log level
+        Gdx.gl.glClearColor(1, 0 , 0,1);
+
+        // Set Libgdx log level
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 
 		// Load assets
@@ -39,6 +43,14 @@ public class EscapeRouteMain extends ApplicationAdapter {
 
 		// Start game at menu screen
 		ScreenTransition transition = ScreenTransitionSlice.init(2, ScreenTransitionSlice.UP_DOWN, 10, Interpolation.pow5Out);
-		setScreen(new MenuScreen(this), transition);
+		// setScreen(new MenuScreen(this), transition);
 	}
+
+    @Override
+    public void render() {
+        //super.render();
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        gsm.update(Gdx.graphics.getDeltaTime());
+        gsm.render(batch);
+    }
 }
