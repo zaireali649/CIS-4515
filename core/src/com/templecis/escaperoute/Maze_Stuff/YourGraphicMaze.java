030275a1b3efca54b6ec3dbc33f219a64afaa085
AnthonyJeffreyHo <35696893+AnthonyJeffreyHo@users.noreply.github.com>
2018-04-25 11:51:57 -0400

    Started on grabbing cell info from code

--- core/src/com/templecis/escaperoute/Maze_Stuff/YourGraphicMaze.java
+++ core/src/com/templecis/escaperoute/Maze_Stuff/YourGraphicMaze.java
@@ -5,6 +5,9 @@ package com.templecis.escaperoute.Maze_Stuff;
  */
 
 // Breath-first  traversal for the path.
+import com.badlogic.gdx.utils.Array;
+import com.templecis.escaperoute.game.objects.MazeTile;
+
 import java.util.*;
 import java.awt.Point;
 import java.util.LinkedList;
@@ -38,20 +41,42 @@ public class YourGraphicMaze {
 
     // Creates the path through maze, starting at cell (srow, scol)
     // and ending at cell (erow and ecol),  in L
-    public static boolean CreatePath(Maze maze, int srow, int scol, int erow, int ecol, LinkedList<Point> L) {
+
+
+    public Array<MazeTile> maze_tiles_up;
+    public Array<MazeTile> maze_tiles_down;
+    public Array<MazeTile> maze_tiles_left;
+    public Array<MazeTile> maze_tiles_right;
+
+    //then merge all directions into one array
+
+
+    public boolean CreatePath(Maze maze, int srow, int scol, int erow, int ecol, LinkedList<Point> L) {
         boolean done = false;
         int c = scol;
         int r = srow;
         //        V[srow][scol] = 1;
         V[r][c] = 1;
 
+
+
+
         if (!(done) && (r > 1) && (V[r - 1][c] != 1) && maze.can_go(r, c, 'U')) {
+            MazeTile mz = new MazeTile();
+            mz.position.x = r;
+            mz.position.y = c;
+            mz.topWall = maze.can_go(r, c, 'U');
+            maze_tiles_up.add(mz);
+
             r--;
             done = CreatePath(maze, r, c, erow, ecol, L);
             r++;
         }
 
         if (!(done) && (c < C) && (V[r][c + 1] != 1) && maze.can_go(r, c, 'R')) {
+
+
+
             c++;
             done = CreatePath(maze, r, c, erow, ecol, L);
             c--;
