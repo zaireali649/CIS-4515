a6d511ba4afb5b6b08fbe09a94f02c5396b53079
PeterM1 <MarchToOctober@gmail.com>
2018-04-30 02:05:38 -0400

    Begin Testing

---
+++ core/test/com/templecis/escaperoute/game/WorldRendererTest.java
@@ -0,0 +1,19 @@
+package com.templecis.escaperoute.game;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by petermontanez on 4/29/18.
+ */
+public class WorldRendererTest {
+
+    @Test
+    public void sample(){
+
+       // WorldRenderer worldRenderer = new WorldRenderer()
+        assertEquals("bananas", "bananas");
+    }
+
+}
